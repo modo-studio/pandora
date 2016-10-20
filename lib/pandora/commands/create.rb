30610f2a89080b69c3fc45abdaf685f9047ede2c
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-10-20 23:02:24 +0200

    Update Travis script

--- lib/pandora/commands/create.rb
+++ lib/pandora/commands/create.rb
@@ -12,12 +12,16 @@ module Pandora
       # @param [String] directory where the framework will be created.
       # @param [String] framework name.
       # @param [String] framework organization.
+      # @param [Bool] open the project once the command finishes.
+      # @param [Bool] verbose.
       # @return [Create] initialized command.
-      def initialize(path, name, organization)
+      def initialize(path, name, organization, open_when_completed = false, verbose = true)
         @path = path
         @name = name
         @organization = organization
         @url = "https://github.com/frameworkoriented/template/archive/master.zip"
+        @open_when_completed = open_when_completed
+        @verbose = verbose
       end
 
       # Executes the command
@@ -26,25 +30,24 @@ module Pandora
         self.unzip_template
         self.rename_files(self.framework_path)
         self.rename_files_content
-        puts "Enjoy #{@name} FRAMEWORK! #yatusabes".colorize(:light_yellow)
-        system "pedropinerabuendia.xcuserdatad"
-        system "open #{project_path}"
+        puts "Enjoy #{@name} FRAMEWORK! #yatusabes".colorize(:light_yellow) if @verbose
+        system "open #{project_path}" if @open_when_completed
       end
 
       protected
 
       def download_template
-        puts "=> Downloading framework template".colorize(:green)
+        puts "=> Downloading framework template".colorize(:green) if @verbose
         response = RestClient.get(@url)
         File.delete(self.zip_path) if File.exist?(self.zip_path)
         zip_file = File.new(self.zip_path, "wb")
         zip_file << response.body
         zip_file.close
-        puts "=> Framework template downloaded".colorize(:green)
+        puts "=> Framework template downloaded".colorize(:green) if @verbose
       end
 
       def unzip_template
-        puts "=> Uncompressing template".colorize(:green)
+        puts "=> Uncompressing template".colorize(:green) if @verbose
         Zip::File.open(self.zip_path) do |zip_file|
           zip_file.each do |f|
             fpath = File.join(@path, f.name)
@@ -54,7 +57,7 @@ module Pandora
         FileUtils.remove(self.zip_path) if File.exist?(self.zip_path)
         FileUtils.remove_dir self.framework_path if File.exist?(self.framework_path)
         FileUtils.mv File.join(@path, "template-master"), self.framework_path
-        puts '=> Template uncompressed'.colorize(:green)
+        puts '=> Template uncompressed'.colorize(:green) if @verbose
       end
 
       def rename_files(path)
@@ -68,7 +71,7 @@ module Pandora
       end
 
       def rename_files_content
-        puts "=> Renaming files content".colorize(:green)
+        puts "=> Renaming files content".colorize(:green) if @verbose
         Dir[File.join(self.framework_path, "**/*")]
             .select { |fn| !File.directory?(fn) }
             .each do |file_path|
@@ -77,7 +80,7 @@ module Pandora
           new_contents = new_contents.gsub("YYYYY", @organization)
           File.open(file_path, "w") {|file| file.puts new_contents }
         end
-        puts "=> Contents renamed".colorize(:green)
+        puts "=> Contents renamed".colorize(:green) if @verbose
       end
 
       def project_path
